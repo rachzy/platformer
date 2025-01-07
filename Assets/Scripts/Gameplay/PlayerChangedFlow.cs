190180e933e32cb6367f836c9cddeb39f7a41a50
rachzy <henriqueferreira.fox@gmail.com>
2025-01-06 22:51:50 -0300

    refactor: remove debug logs

--- Assets/Scripts/Gameplay/PlayerChangedFlow.cs
+++ Assets/Scripts/Gameplay/PlayerChangedFlow.cs
@@ -16,16 +16,12 @@ namespace Platformer.Gameplay
             switch (player.levelManager.currentFlow)
             {
                 case Flow.ReversedGravity:
-                    Debug.Log("Reversing gravity");
                     Physics2D.gravity = defaultGravity * -1;
                     player.castDistance *= -1;
                     break;
                 default:
                     if (player.currentFlow == Flow.ReversedGravity)
                     {
-                        Debug.Log("Setting back gravity");
-                        Debug.Log(Physics2D.gravity);
-                        Debug.Log(defaultGravity);
                         Physics2D.gravity = defaultGravity;
                         player.castDistance *= -1;
                     }
